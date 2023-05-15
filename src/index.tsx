c694c552490f26230233f890233df0cd6ae4c5b4
mertyldrr <mertyildirir14@gmail.com>
2023-05-15 23:34:51 +0200

    initialize redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import "./input.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import newsReducer from "./redux/slices/newsSlice";
+
+export const store = configureStore({
+  reducer: {
+    news: newsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
