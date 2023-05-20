47d5cb27a53c0f704d468bf4c7dc924221ea8ff5
mertyldrr <mertyildirir14@gmail.com>
2023-05-20 16:32:01 +0200

    optimize slices

--- src/redux/slices/appSlice.ts
+++ src/redux/slices/appSlice.ts
@@ -27,14 +27,12 @@ const appSlice = createSlice({
     },
     removeApp(state, action: PayloadAction<string>) {
       const idToRemove = action.payload;
-      state.apps = state.apps.filter((app) => app && app.id !== idToRemove);
+      state.apps = state.apps.filter((app) => app.id !== idToRemove);
     },
-    minimizeApp(state, action: PayloadAction<string>) {
+    toggleMinimizeApp(state, action: PayloadAction<string>) {
       const idToMinimize = action.payload;
       // find the app
-      const appIndex = state.apps.findIndex(
-        (app) => app && app.id === idToMinimize
-      );
+      const appIndex = state.apps.findIndex((app) => app.id === idToMinimize);
       if (appIndex !== -1) {
         // update minimized value
         state.apps[appIndex].isMinimized = !state.apps[appIndex].isMinimized;
@@ -54,7 +52,12 @@ const appSlice = createSlice({
   },
 });
 
-export const { addApp, removeApp, minimizeApp, maximizeApp } = appSlice.actions;
+export const { addApp, removeApp, toggleMinimizeApp, maximizeApp } =
+  appSlice.actions;
 export default appSlice.reducer;
 
 export const selectApps = (state: RootState) => state.apps.apps;
+export const selectMinimized = (state: RootState, id: string) =>
+  state.apps.apps.find((app) => app.id === id)?.isMinimized;
+export const selectIsSelected = (state: RootState, appName: string) =>
+  state.apps.apps.find((app) => app.name === appName)?.isSelected;
