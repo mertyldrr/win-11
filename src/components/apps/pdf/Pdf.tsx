7870bfff8920c6980914496e4e1b1cae9816182e
mertyldrr <mertyildirir14@gmail.com>
2023-05-20 17:50:56 +0200

    use object tag to display pdf

--- src/components/apps/pdf/Pdf.tsx
+++ src/components/apps/pdf/Pdf.tsx
@@ -1,11 +1,16 @@
 export const Pdf = () => {
   return (
     <div className="flex flex-col w-full h-full bg-slate-200">
-      <iframe
-        title="pdf"
-        src={"https://static.mertyildirir.com/media/cv/mert_yildirir_cv.pdf"}
+      <object
         className="w-full h-full rounded-lg"
-      />
+        data="https://static.mertyildirir.com/media/cv/mert_yildirir_cv.pdf"
+        type="application/pdf"
+      >
+        <iframe
+          title="pdf"
+          src="https://docs.google.com/viewer?url=https://static.mertyildirir.com/media/cv/mert_yildirir_cv.pdf&embedded=true"
+        />
+      </object>
     </div>
   );
 };
