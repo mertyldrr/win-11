26effbcc9368ec55c3f1aada7395c73c0b2e8d1f
mertyldrr <mertyildirir14@gmail.com>
2023-05-25 02:45:13 +0200

    use app list to prevent code duplication

--- src/components/taskbar/startmenu/StartMenu.tsx
+++ src/components/taskbar/startmenu/StartMenu.tsx
@@ -1,31 +1,9 @@
-import netflix from "../../../assets/taskbar/netflixLogo.svg";
-import spotify from "../../../assets/taskbar/spotifyLogo.svg";
-
-import edge from "../../../assets/taskbar/edge.png";
-import fileExplorer from "../../../assets/taskbar/fileExplorer.png";
-import settings from "../../../assets/taskbar/settings.png";
-import winStore from "../../../assets/taskbar/winStore.png";
-
-import calculator from "../../../assets/startmenu/calculator.png";
-import camera from "../../../assets/startmenu/camera.png";
-import whiteboard from "../../../assets/startmenu/board.png";
-import mail from "../../../assets/startmenu/mail.png";
-import github from "../../../assets/startmenu/github.png";
-import xbox from "../../../assets/startmenu/xbox.png";
-import teams from "../../../assets/startmenu/teams.png";
-import terminal from "../../../assets/startmenu/terminal.png";
-import paint from "../../../assets/startmenu/paint.png";
-import discord from "../../../assets/startmenu/discord.png";
-import twitter from "../../../assets/startmenu/twitter.png";
-import notepad from "../../../assets/startmenu/notepad.png";
 import psyduck from "../../../assets/startmenu/psyduck.png";
-
 import { FaChevronRight } from "react-icons/fa";
 import { AiOutlinePoweroff } from "react-icons/ai";
-
 import { useSelector } from "react-redux";
 import { selectIsStartModalActive } from "../../../redux/slices/startMenuSlice";
-
+import { pinnedApps, recommendedApps } from "../../../constants";
 import "./StartMenu.css";
 
 export const StartMenu = () => {
@@ -37,123 +15,45 @@ export const StartMenu = () => {
       }`}
     >
       <div className="px-8">
-        <div className="flex flex-row justify-between items-center px-8">
-          <span className="font-medium text-[14px]">Pinned</span>
-          <div className="bg-white cursor-pointer px-2 py-0.5 rounded-sm flex flex-row items-center justify-between shadow-md">
-            <span className="text-[10px]">All apps</span>
+        <div className="flex justify-between items-center px-8">
+          <span className="font-medium text-sm">Pinned</span>
+          <div className="bg-white cursor-pointer px-2 py-0.5 rounded-sm flex items-center justify-between shadow-md">
+            <span className="text-xs">All apps</span>
             <FaChevronRight className="w-4 h-2" />
           </div>
         </div>
-        <div className="flex flex-row flex-wrap justify-between items-center py-6">
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={edge} alt="" />
-            <p className="pinned-app-text">Browser</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={calculator} alt="" />
-            <p className="pinned-app-text">Calculator</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={settings} alt="" />
-            <p className="pinned-app-text">Settings</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={camera} alt="" />
-            <p className="pinned-app-text">Camera</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={fileExplorer} alt="" />
-            <p className="pinned-app-text">File Explorer</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={spotify} alt="" />{" "}
-            <p className="pinned-app-text">Spotify</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={netflix} alt="" />{" "}
-            <p className="pinned-app-text">Netflix</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={whiteboard} alt="" />
-            <p className="pinned-app-text">Whiteboard</p>
-          </button>
-          <button className="pinned-app  hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={twitter} alt="" />
-            <p className="pinned-app-text">Twitter</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={xbox} alt="" />
-            <p className="pinned-app-text">Xbox</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={terminal} alt="" />
-            <p className="pinned-app-text">Terminal</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={discord} alt="" />
-            <p className="pinned-app-text">Discord</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={mail} alt="" />
-            <p className="pinned-app-text">Mail</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={github} alt="" />
-            <p className="pinned-app-text">GitHub</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={paint} alt="" />
-            <p className="pinned-app-text">Paint</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={winStore} alt="" />
-            <p className="pinned-app-text">Store</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={teams} alt="" />
-            <p className="pinned-app-text">Teams</p>
-          </button>
-          <button className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm">
-            <img className="w-8 h-8" src={notepad} alt="" />
-            <p className="pinned-app-text">Notepad</p>
-          </button>
+        <div className="flex flex-wrap justify-between items-center py-6">
+          {pinnedApps.map((app) => (
+            <button
+              className="pinned-app hover:bg-white hover:bg-opacity-50 rounded-sm"
+              key={app.name}
+            >
+              <img className="w-8 h-8" src={app.icon} alt="" />
+              <p className="pinned-app-text">{app.name}</p>
+            </button>
+          ))}
         </div>
       </div>
       <div className="px-12">
-        <div className="flex flex-row justify-between items-center px-4">
-          <span className="font-medium text-[14px]">Recommended</span>
+        <div className="flex justify-between items-center px-4">
+          <span className="font-medium text-sm">Recommended</span>
         </div>
-        <div className="grid grid-cols-2 place-items-center gap-x-4 py-4">
-          <button className="hover:bg-white hover:bg-opacity-50 rounded-sm w-full flex justify-start items-center px-4 py-3">
-            <img className="w-8 h-8" src={mail} alt="" />
-            <p className="recommended-app-text">Mail</p>
-          </button>
-          <button className="hover:bg-white hover:bg-opacity-50 rounded-sm w-full flex justify-start items-center px-4 py-3">
-            <img className="w-8 h-8" src={github} alt="" />
-            <p className="recommended-app-text">GitHub</p>
-          </button>
-          <button className="hover:bg-white hover:bg-opacity-50 rounded-sm w-full flex justify-start items-center px-4 py-3">
-            <img className="w-8 h-8" src={xbox} alt="" />
-            <p className="recommended-app-text">Xbox</p>
-          </button>
-          <button className="hover:bg-white hover:bg-opacity-50 rounded-sm w-full flex justify-start items-center px-4 py-3">
-            <img className="w-8 h-8" src={winStore} alt="" />
-            <p className="recommended-app-text">Store</p>
-          </button>
-          <button className="hover:bg-white hover:bg-opacity-50 rounded-sm w-full flex justify-start items-center px-4 py-3">
-            <img className="w-8 h-8" src={twitter} alt="" />
-            <p className="recommended-app-text">Twitter</p>
-          </button>
-          <button className="hover:bg-white hover:bg-opacity-50 rounded-sm w-full flex justify-start items-center px-4 py-3">
-            <img className="w-8 h-8" src={spotify} alt="" />
-            <p className="recommended-app-text">Spotify</p>
-          </button>
+        <div className="grid grid-cols-2 gap-x-4 py-4">
+          {recommendedApps.map((app) => (
+            <button
+              className="hover:bg-white hover:bg-opacity-50 rounded-sm w-full flex justify-start items-center px-4 py-3"
+              key={app.name}
+            >
+              <img className="w-8 h-8" src={app.icon} alt="" />
+              <p className="recommended-app-text">{app.name}</p>
+            </button>
+          ))}
         </div>
       </div>
-      <div className="px-12 flex flex-row justify-between w-full border-t-[1px] py-2 border-gray-400 bg-gray-100 bg-opacity-60 rounded-b-lg">
-        <div className="hover:bg-white hover:bg-opacity-50 rounded-sm p-4 py-2 flex flex-row justify-between items-center">
+      <div className="px-12 flex justify-between w-full border-t border-gray-400 bg-gray-100 bg-opacity-60 rounded-b-lg py-2">
+        <div className="hover:bg-white hover:bg-opacity-50 rounded-sm p-4 py-2 flex justify-between items-center">
           <img className="w-8 h-8" src={psyduck} alt="" />
-          <p className="text-[12px] ml-2">Mert</p>
+          <p className="text-xs ml-2">Mert</p>
         </div>
         <button className="hover:bg-white hover:bg-opacity-50 rounded-sm px-4 py-2">
           <AiOutlinePoweroff className="w-6 h-6" />
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,55 @@
+import netflix from "./assets/taskbar/netflixLogo.svg";
+import spotify from "./assets/taskbar/spotifyLogo.svg";
+
+import edge from "./assets/taskbar/edge.png";
+import fileExplorer from "./assets/taskbar/fileExplorer.png";
+import settings from "./assets/taskbar/settings.png";
+import winStore from "./assets/taskbar/winStore.png";
+
+import calculator from "./assets/startmenu/calculator.png";
+import camera from "./assets/startmenu/camera.png";
+import whiteboard from "./assets/startmenu/board.png";
+import mail from "./assets/startmenu/mail.png";
+import github from "./assets/startmenu/github.png";
+import xbox from "./assets/startmenu/xbox.png";
+import teams from "./assets/startmenu/teams.png";
+import terminal from "./assets/startmenu/terminal.png";
+import paint from "./assets/startmenu/paint.png";
+import discord from "./assets/startmenu/discord.png";
+import twitter from "./assets/startmenu/twitter.png";
+import notepad from "./assets/startmenu/notepad.png";
+
 export const sandboxUrl =
   "https://codesandbox.io/p/github/mertyldrr/win-11/master?layout=%257B%2522sidebarPanel%2522%253A%2522EXPLORER%2522%252C%2522rootPanelGroup%2522%253A%257B%2522direction%2522%253A%2522horizontal%2522%252C%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522id%2522%253A%2522ROOT_LAYOUT%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL%2522%252C%2522panelType%2522%253A%2522TABS%2522%252C%2522id%2522%253A%2522clhw4lmhg00is3t6nifej3tkg%2522%257D%255D%252C%2522sizes%2522%253A%255B100%255D%257D%252C%2522tabbedPanels%2522%253A%257B%2522clhw4lmhg00is3t6nifej3tkg%2522%253A%257B%2522id%2522%253A%2522clhw4lmhg00is3t6nifej3tkg%2522%252C%2522tabs%2522%253A%255B%257B%2522type%2522%253A%2522TASK_PORT%2522%252C%2522taskId%2522%253A%2522start%2522%252C%2522port%2522%253A3000%252C%2522id%2522%253A%2522clhw4kcbk00c23t6n1bhaut0p%2522%252C%2522mode%2522%253A%2522permanent%2522%252C%2522path%2522%253A%2522%252F%2522%257D%255D%252C%2522activeTabId%2522%253A%2522clhw4kcbk00c23t6n1bhaut0p%2522%257D%257D%252C%2522showDevtools%2522%253Atrue%252C%2522showSidebar%2522%253Atrue%252C%2522sidebarPanelSize%2522%253A15%257D";
 
 export const defaultBrowserUrl = "https://www.bing.com";
+
+export const pinnedApps = [
+  { name: "Browser", icon: edge },
+  { name: "Calculator", icon: calculator },
+  { name: "Settings", icon: settings },
+  { name: "Camera", icon: camera },
+  { name: "File Explorer", icon: fileExplorer },
+  { name: "Spotify", icon: spotify },
+  { name: "Netflix", icon: netflix },
+  { name: "Whiteboard", icon: whiteboard },
+  { name: "Twitter", icon: twitter },
+  { name: "Xbox", icon: xbox },
+  { name: "Terminal", icon: terminal },
+  { name: "Discord", icon: discord },
+  { name: "Mail", icon: mail },
+  { name: "GitHub", icon: github },
+  { name: "Paint", icon: paint },
+  { name: "Store", icon: winStore },
+  { name: "Teams", icon: teams },
+  { name: "Notepad", icon: notepad },
+];
+
+export const recommendedApps = [
+  { name: "Mail", icon: mail },
+  { name: "GitHub", icon: github },
+  { name: "Xbox", icon: xbox },
+  { name: "Store", icon: winStore },
+  { name: "Twitter", icon: twitter },
+  { name: "Spotify", icon: spotify },
+];
