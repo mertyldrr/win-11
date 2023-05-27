ace325679e4e19ebdddd95fe3dea1884d7405e18
mertyldrr <mertyildirir14@gmail.com>
2023-05-28 00:25:56 +0200

    add gmail and github to the bookmarks

--- src/components/apps/edge/Bookmarks.tsx
+++ src/components/apps/edge/Bookmarks.tsx
@@ -1,9 +1,11 @@
 import { bookmarks } from "../../../constants";
 import { Bookmark } from "./Bookmark";
+import github from "../../../assets/startmenu/github.png";
+import gmail from "../../../assets/bookmarks/gmail.svg";
 
 export const Bookmarks = () => {
   return (
-    <div className="flex flex-row items-center text-[0.75rem] h-12 space-x-4 ml-2">
+    <div className="flex flex-row items-center text-[0.75rem] h-12 space-x-2 ml-2">
       {bookmarks.map((bookmark, index) => (
         <Bookmark
           key={index}
@@ -12,6 +14,24 @@ export const Bookmarks = () => {
           icon={bookmark.icon}
         />
       ))}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/mertyldrr"
+        className="flex flex-row items-center space-x-2 hover:bg-gray-400 hover:bg-opacity-30 p-2 rounded-sm cursor-default"
+      >
+        <img className="w-5 h-5" src={github} alt="" />
+        <span>GitHub</span>
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="mailto:mertyildirir14@gmail.com"
+        className="flex flex-row items-center space-x-2 hover:bg-gray-400 hover:bg-opacity-30 p-2 rounded-sm cursor-default"
+      >
+        <img className="w-5 h-5" src={gmail} alt="" />
+        <span>Gmail</span>
+      </a>
     </div>
   );
 };
