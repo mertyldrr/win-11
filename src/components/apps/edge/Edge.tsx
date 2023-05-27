9150de42e8176546f9053d63cae098f5e61b6343
mertyldrr <mertyildirir14@gmail.com>
2023-05-28 00:26:50 +0200

    disable forward and back buttons dynamically based on history

--- src/components/apps/edge/Edge.tsx
+++ src/components/apps/edge/Edge.tsx
@@ -1,14 +1,20 @@
 import account from "../../../assets/ui/defAccount.png";
 import arrowClockwise from "../../../assets/ui/arrowClockwise.svg";
-import arrowLeft from "../../../assets/ui/arrowLeft.svg";
+import { VscArrowLeft } from "react-icons/vsc";
+import { VscArrowRight } from "react-icons/vsc";
+
 import { Bookmarks } from "./Bookmarks";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  selectSearchText,
-  setSearchText,
-} from "../../../redux/slices/searchSlice";
 import { useEffect, useState } from "react";
 import { SearchInput } from "../../SearchInput";
+import {
+  goBack,
+  goForward,
+  selectIsBackActive,
+  selectIsForwardActive,
+  selectSearchText,
+  setBrowserHistory,
+} from "../../../redux/slices/browserSlice";
 
 interface EdgeProps {
   url?: string;
@@ -17,16 +23,19 @@ interface EdgeProps {
 export const Edge: React.FC<EdgeProps> = ({ url }) => {
   const [localSearchText, setLocalSearchText] = useState<string>("");
   const searchText = useSelector(selectSearchText);
+  const isForwardActive = useSelector(selectIsForwardActive);
+  const isBackActive = useSelector(selectIsBackActive);
+
   const dispatch = useDispatch();
 
-  const debouncedSearchTextUpdate = (searchText: string) => {
+  const searchTextUpdate = (searchText: string) => {
     setLocalSearchText(searchText);
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      dispatch(setSearchText(localSearchText));
+      dispatch(setBrowserHistory(localSearchText));
     }
   };
 
@@ -39,7 +48,7 @@ export const Edge: React.FC<EdgeProps> = ({ url }) => {
 
   useEffect(() => {
     if (url) {
-      dispatch(setSearchText(url));
+      dispatch(setBrowserHistory(url));
     }
   }, [dispatch, url]);
 
@@ -47,9 +56,23 @@ export const Edge: React.FC<EdgeProps> = ({ url }) => {
     <div className="flex flex-col w-full h-full bg-slate-200 rounded-b-lg">
       <div className="w-full flex flex-row justify-between items-center h-12">
         <div className="flex flex-row justify-center items-center h-4/5 space-x-2 mx-2">
-          <button className="h-full cursor-default hover:bg-gray-400 hover:bg-opacity-30 px-2 rounded-sm">
-            <img src={arrowLeft} alt="" />
+          <button
+            disabled={!isBackActive ? true : false}
+            className="h-full cursor-default hover:bg-gray-400 hover:bg-opacity-30 px-2 rounded-sm disabled:bg-slate-200 disabled:opacity-30"
+            onClick={() => dispatch(goBack())}
+          >
+            {/* <img src={arrowLeft} alt="" /> */}
+            <VscArrowLeft size={20} />
           </button>
+
+          <button
+            disabled={!isForwardActive ? true : false}
+            className="h-full cursor-default hover:bg-gray-400 hover:bg-opacity-30 px-2 rounded-sm disabled:bg-slate-200 disabled:opacity-30"
+            onClick={() => dispatch(goForward())}
+          >
+            <VscArrowRight size={20} />
+          </button>
+
           <button className="h-full cursor-default hover:bg-gray-400 hover:bg-opacity-30 px-2 rounded-sm">
             <img src={arrowClockwise} alt="" />
           </button>
@@ -60,7 +83,7 @@ export const Edge: React.FC<EdgeProps> = ({ url }) => {
               twClassName={"text-sm w-full h-full outline-none"}
               type={"search"}
               value={localSearchText}
-              onChange={(e) => debouncedSearchTextUpdate(e.target.value)}
+              onChange={(e) => searchTextUpdate(e.target.value)}
               onKeyDown={handleKeyDown}
             />
           </div>
